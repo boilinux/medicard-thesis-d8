8bd5a21a97730ab35c6f446d4d36167add4ed488
stephenjohnwenceslao <stephenjohnwenceslao@gmail.com>
2018-01-05 20:04:26 +0800

    Update for medicard.

--- www/modules/custom/custom_general/src/Controller/userDashboard.php
+++ www/modules/custom/custom_general/src/Controller/userDashboard.php
@@ -4,7 +4,6 @@ namespace Drupal\custom_general\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\custom_general\Controller\apiHelper;
-use Drupal\custom_general\Controller\apiLoan;
 
 class userDashboard extends ControllerBase {
 
@@ -13,55 +12,6 @@ class userDashboard extends ControllerBase {
    */
   public function user_dashboard() {
     $output = "";
-
-    if (!apiHelper::check_user_role('loan')) {
-      $output .= '
-      <div class="portlet">
-        <div class="portlet-title">
-          <span class="caption-subject theme-font bold uppercase">Sales Summary</span>
-        </div>
-        <div class="portlet-body">
-            <div class="row list-separated">
-              <div class="col-md-3 col-sm-3 col-xs-6">
-                <div class="font-grey-mint font-sm">
-                   Total Sales
-                </div>
-                <div class="uppercase font-hg font-red-flamingo">
-                   13,760 <span class="font-lg font-grey-mint">$</span>
-                </div>
-              </div>
-              <div class="col-md-3 col-sm-3 col-xs-6">
-                <div class="font-grey-mint font-sm">
-                   Revenue
-                </div>
-                <div class="uppercase font-hg theme-font">
-                   4,760 <span class="font-lg font-grey-mint">$</span>
-                </div>
-              </div>
-              <div class="col-md-3 col-sm-3 col-xs-6">
-                <div class="font-grey-mint font-sm">
-                   Expenses
-                </div>
-                <div class="uppercase font-hg font-purple">
-                   11,760 <span class="font-lg font-grey-mint">$</span>
-                </div>
-              </div>
-              <div class="col-md-3 col-sm-3 col-xs-6">
-                <div class="font-grey-mint font-sm">
-                   Growth
-                </div>
-                <div class="uppercase font-hg font-blue-sharp">
-                   9,760 <span class="font-lg font-grey-mint">$</span>
-                </div>
-              </div>
-            </div>
-          </div>
-      </div>
-      <div id="sales_statistics" class="portlet-body-morris-fit morris-chart" style="height: 260px">';
-
-    }
-    else {
-      $output = "<div class='col-sm-12'><h1>Active Loan</h1></div>";
       $accordion = '<div class="panel-group accordion" id="archives">
         <div class="panel">
           <div class="panel-heading">
@@ -71,89 +21,54 @@ class userDashboard extends ControllerBase {
             </h4>
           </div>';
       $output2 = "";
-      $archives = array();
 
-      $query_loan = \Drupal::database()->query("SELECT nfd.nid AS nid, nfn.field_name_value AS name, nfa.field_archive_value AS archive FROM node_field_data AS nfd
-        LEFT JOIN node__field_name AS nfn ON nfn.entity_id = nfd.nid
-        LEFT JOIN node__field_archive AS nfa ON nfa.entity_id = nfd.nid
-        WHERE nfd.type = 'loan' AND nfd.uid = " . \Drupal::currentUser()->id() . " ORDER BY nfd.created DESC")->fetchAll();
+    if (apiHelper::check_user_role('nurse')) {
+      $query = \Drupal::database()->query("SELECT nffn.field_first_name_value AS firstname, nfln.field_last_name_value AS lastname, nfd.created AS created, nfd.nid AS nid FROM node_field_data AS nfd 
+        LEFT JOIN node__field_first_name AS nffn ON nffn.entity_id = nfd.nid
+        LEFT JOIN node__field_last_name As nfln ON nfln.entity_id = nfd.nid
+        WHERE nfd.type = 'patient' AND nfd.uid = " . \Drupal::currentUser()->id() . " ORDER BY nfd.created DESC")->fetchAll();
       
-      foreach ($query_loan as $res) {
-        if ($res->archive != 1) {
-          $output .= "<div class='col-md-6 col-sm-12'><div class='portlet yellow-crusta box'>";
+      foreach ($query as $res) {
+        $output .= "<div class='col-md-6 col-sm-12'><div class='portlet yellow-crusta box'>";
 
-          $output .= "<div class='portlet-title'><div class='caption'><i class='fa fa-user'></i> " . ucwords($res->name) . "</div><div class='actions'><a class='btn btn-default btn-sm' href='/user/loan/" . $res->nid . "'><i class='fa fa-info-circle'></i> View</a><a class='btn btn-default btn-sm' href='#loan_modal" . $res->nid . "' data-toggle='modal'><i class='fa fa-archive'></i> Archive</a></div></div>";
+        $output .= "<div class='portlet-title'><div class='caption'><i class='fa fa-user'></i> " . ucwords($res->firstname) . " " . ucwords($res->lastname) . "</div><div class='actions'><a class='btn btn-default btn-sm' href='/user/loan/" . $res->nid . "'><i class='fa fa-info-circle'></i> View</a><a class='btn btn-default btn-sm' href='/update/patient/" . $res->nid . "'><i class='fa fa-edit'></i> edit</a></div></div>";
 
-          $output .= "<div class='portlet-body'>
-            <div class='row static-info'>
-              <div class='col-md-5 name'>Loan amount:</div><div class='col-md-7 value'>" . apiLoan::loan_amount($res->nid) . "</div>
-            </div>
-            <div class='row static-info'>
-              <div class='col-md-5 name'>Balance:</div><div class='col-md-7 value'>" . apiLoan::loan_balance($res->nid) . "</div>
-            </div>
-            <div class='row static-info'>
-              <div class='col-md-5 name'>Paid:</div><div class='col-md-7 value'>" . apiLoan::loan_paid_total($res->nid) . "</div>
-            </div>
-            <div class='row static-info'>
-              <div class='col-md-5 name'>Payment start:</div><div class='col-md-7 value'>" . apiLoan::loan_payment_start($res->nid) . "</div>
-            </div>
-          </div>";
+        $output .= "<div class='portlet-body'>
+          <div class='row static-info'>
+            <div class='col-md-5 name'>Created:</div><div class='col-md-7 value'> " . date("d-M-Y", $res->created) . "</div>
+          </div>
+          <div class='row static-info'>
+          </div>
+        </div>";
 
-          $output .= '<div class="modal fade" id="loan_modal' . $res->nid . '" tabindex="-1" role="basic" aria-hidden="true">
-                <div class="modal-dialog">
-                  <div class="modal-content">
-                    <div class="modal-header">
-                      <button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
-                      <h4 class="modal-title">Please confirm</h4>
-                    </div>
-                    <div class="modal-body">
-                       <span class="color_green">' . ucwords($res->name) . '</span> loan will be archive.
-                    </div>
-                    <div class="modal-footer">
-                      <a type="button" class="btn default" data-dismiss="modal">Close</a>
-                      <a href="/user/loan/' . $res->nid . '/archive" class="btn blue">Submit</a>
-                    </div>
+        $output .= '<div class="modal fade" id="loan_modal' . $res->nid . '" tabindex="-1" role="basic" aria-hidden="true">
+              <div class="modal-dialog">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
+                    <h4 class="modal-title">Please confirm</h4>
                   </div>
-                  <!-- /.modal-content -->
+                  <div class="modal-body">
+                     <span class="color_green">' . ucwords($res->name) . '</span> loan will be archive.
+                  </div>
+                  <div class="modal-footer">
+                    <a type="button" class="btn default" data-dismiss="modal">Close</a>
+                    <a href="/user/loan/' . $res->nid . '/archive" class="btn blue">Submit</a>
+                  </div>
                 </div>
-                <!-- /.modal-dialog -->
-              </div>';
+                <!-- /.modal-content -->
+              </div>
+              <!-- /.modal-dialog -->
+            </div>';
 
-          $output .= "</div></div>";
-        }
-        else {
-          $output2 .= "<div class='col-md-6 col-sm-12'><div class='portlet red box'>";
-
-          $output2 .= "<div class='portlet-title'><div class='caption'><i class='fa fa-user'></i> " . ucwords($res->name) . "</div><div class='actions'><a class='btn btn-default btn-sm' href='/user/loan/" . $res->nid . "'><i class='fa fa-info-circle'></i> View</a></div></div>";
-
-          $output2 .= "<div class='portlet-body'>
-            <div class='row static-info'>
-              <div class='col-md-5 name'>Loan amount:</div><div class='col-md-7 value'>" . apiLoan::loan_amount($res->nid) . "</div>
-            </div>
-            <div class='row static-info'>
-              <div class='col-md-5 name'>Balance:</div><div class='col-md-7 value'>" . apiLoan::loan_balance($res->nid) . "</div>
-            </div>
-            <div class='row static-info'>
-              <div class='col-md-5 name'>Paid:</div><div class='col-md-7 value'>" . apiLoan::loan_paid_total($res->nid) . "</div>
-            </div>
-          </div>";
-
-          $output2 .= "</div></div>";
-        }
+        $output .= "</div></div>";
       }
-
-
-
-      if (empty($query_loan)) {
-        $output .= "<div class='col-sm-12'>Please add new loan <a href='/user/add_loan'>here</a>.</div>";
-      }
-
-      $accordion .= '<div id="collapse1" class="panel-collapse collapse">
-          <div class="panel-body row">' . $output2 . '</div>
-        </div>
-      </div>';
     }
 
-    return array('#markup' => "<div class='row'>" . $output . "</div<div class='row'>" . $accordion . "</div", '#cache' => ['max-age' => 0,]);
+    if (empty($query)) {
+      $output .= "<div class='col-sm-12'>No patient yet.</div>";
+    }
+
+    return array('#markup' => "<div class='row'>" . $output . "</div>", '#cache' => ['max-age' => 0,]);
   }
 }
